b67c7297b4b2f9b578731518f8d83b7380077dfb
Krisjanis Balodis <KrisjanisB1@verifone.com>
2018-11-07 10:40:45 +0200

    end

--- MD_22.hs
+++ MD_22.hs
@@ -7,13 +7,16 @@ mm f (Leaf a) = Leaf (map f a)
 mm f (Node a t1 t2 t3) = Node (map f a) (mm f t1) (mm f t2) (mm f t3)
     
 a :: Integer -> Integer
-a x = x*x
+a x = x * x
 
 b :: Integer -> Integer
 b x = x `mod` 7
 
 c :: Integer -> Integer
 c x = x + 100
+
+tree :: TTT Integer
+tree = (Node [44, -23] (Leaf [5, -14, 12, 13, 5]) (Node [33, 16] (Leaf [3, 2]) (Leaf [6]) (Leaf [7])) (Node [33, 16] (Leaf [3, 2]) (Leaf [-6]) (Leaf [7])))
 
 ff_a :: TTT Integer
 ff_a = mm a (Node [44, -23] (Leaf [5, -14, 12, 13, 5]) (Node [33, 16] (Leaf [3, 2]) (Leaf [6]) (Leaf [7])) (Node [33, 16] (Leaf [3, 2]) (Leaf [-6]) (Leaf [7])))
